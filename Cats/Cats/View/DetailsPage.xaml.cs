87b34eef14450b72fccd3eafb07ba0a2bd8d566c
Vinicios de Lima Clarindo <vinicioslc@outlook.com>
2017-02-24 12:55:15 -0300

    Layout Button and Texts
    Corrigido layout e texto do botão de ir ao wikipedia
    Adicionado comando de abrir o wikipedia

--- Cats/Cats/View/DetailsPage.xaml.cs
+++ Cats/Cats/View/DetailsPage.xaml.cs
@@ -12,14 +12,24 @@ namespace Cats.View
 	public partial class DetailsPage : ContentPage
     {
         //Cat atual
-        Cat SelectedCat;
+        Cat SelectedCat;
+
         //Cat selecionado
         public DetailsPage(Cat selectedCat)
         {
             InitializeComponent();
             this.SelectedCat = selectedCat;
             //contexto de ligação setado para o gato selecionado
-            BindingContext = this.SelectedCat;            
+            BindingContext = this.SelectedCat;
+            ButtonWebSite.Clicked += ButtonWebSite_Clicked;
+        }
+
+        private void ButtonWebSite_Clicked(object sender, EventArgs e)
+        {
+            if (SelectedCat.WebSite.StartsWith("http"))
+            {
+                Device.OpenUri(new Uri(SelectedCat.WebSite));
+            }
         }
     }
 }
